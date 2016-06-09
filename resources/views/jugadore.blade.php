28ee1648a9943db8a008821a4b040f80bcd939f9
Kevin Filella <kfilella@espol.edu.ec>
2016-06-09 11:31:47 -0500

    Asigna valores correctos de fecha_nac/categoria/equipo en Jugador/edit

--- resources/views/jugadore.blade.php
+++ resources/views/jugadore.blade.php
@@ -44,7 +44,7 @@
                     </div>
                     <div class="form-group col-xs-12">
                         <label for="fecha_nac">Fecha Nacimiento</label>
-                        <input type="datetime-local" class="form-control" id="fecha_nac" value="{{ $jugador['fecha_nac'] }}" name="fecha_nac" placeholder="Ingrese fecha de nacimiento">
+                        <input type="date" class="form-control" id="fecha_nac" value="{{ $jugador['fecha_nac'] }}" name="fecha_nac" placeholder="Ingrese fecha de nacimiento">
                     </div>
                     <div class="form-group col-xs-12">
                         <label for="inputCedula">C&eacute;dula</label>
@@ -84,9 +84,12 @@
                         <label for="categoria">Categor&iacute;a</label>
                         <!-- Campo para seleccionar la categoría del jugador -->
                         <select class="form-control input" id="categoria" name="categoria">
-                            <option selected="selected"></option>
                             @foreach($categorias as $categoria)
-                               <option value="{{ $categoria['nombre'] }}">{{ $categoria->nombre}}</option>
+                                @if($categoria->nombre == $jugador->categoria)
+                                    <option selected="selected" value="{{ $categoria['nombre'] }}">{{ $categoria->nombre}}</option>
+                                @else
+                                    <option value="{{ $categoria['nombre'] }}">{{ $categoria->nombre}}</option>
+                                @endif
                             @endforeach
                         </select>
                     </div>
@@ -94,9 +97,12 @@
                         <label for="equipo">Equipo</label>
                         <!-- Campo para seleccionar el equipo para busqueda del jugador -->
                         <select class="form-control input" id="equipo" name="equipo">
-                            <option selected="selected"></option>
                             @foreach($equipos as $equipo)
-                               <option value="{{ $equipo['id'] }}">{{ $equipo->nombre}}</option>
+                                @if($equipo->id == $jugador->id_equipo)
+                                    <option selected="selected" value="{{ $equipo['id'] }}">{{ $equipo->nombre}}</option>
+                                @else
+                                    <option value="{{ $equipo['id'] }}">{{ $equipo->nombre}}</option>
+                                @endif
                             @endforeach
                         </select>
                     </div>   
@@ -107,7 +113,7 @@
                 <div class="col-xs-2"></div>
                 <div class="col-xs-8">
                     <div class="col-xs-4">
-                        <button type="submit" class="btn btn-primary">Limpiar</button>
+                        <button type="button" class="btn btn-primary">Reestablecer</button>
                     </div>
                     <div class="col-xs-4 pull-right">
                         <button type="submit" class="btn btn-success">Actualizar</button>
