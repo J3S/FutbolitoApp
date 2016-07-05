cb556b959abe68c69f92da2ccf6e470b78e7f9d7
Brannybk <brajchit@espol.edu.ec>
2016-07-05 13:09:22 -0500

    eq test

--- tests/CrearEquipoTest.php
+++ tests/CrearEquipoTest.php
@@ -6,13 +6,111 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class CrearEquipoTest extends TestCase
 {
+    // use DatabaseTransactions;
+
     /**
-     * A basic test example.
+     * Comprueba el funcionamiento para crear un Equipo.
+     * Se crea un equipo y para guardar se envia por ajax los datos en formato JSON,
+     * se ingresa nombre alfanumerico, se omite entrenador, categoria master,
+     * sin jugadores.
+     * Es exitoso si el sistema responde con un mensaje 'guadrado con exito'.
+     * Corresponde al caso de prueba testCrearEquipo: post-condition 1.
      *
      * @return void
      */
-    public function testExample()
+    public function testCrearEquipo1()
     {
-        $this->assertTrue(true);
+        $this->post(
+                    '/equipo',
+                    [
+                        'nombre' => 'MiEquipo',
+                        'entrenador' => '',
+                        'categoria' => 'Master',
+                        'ids' => null,
+                    ]
+                    )
+             ->seeJson([
+                 'mensaje' => 'guardado con exito',
+             ]);
+    }
+
+    /**
+     * Comprueba el funcionamiento para crear un Equipo.
+     * Se crea un equipo y para guardar se envia por ajax los datos en formato JSON,
+     * se ingresa nombre alfanumerico(MiEquipo), entrenador solo letras(Jorge),
+     * categoria(Master), sin jugadores.
+     * Es exitoso si el sistema responde con un mensaje 'guadrado con exito'.
+     * Corresponde al caso de prueba testCrearEquipo: post-condition 1.
+     *
+     * @return void
+     */
+    public function testCrearEquipo2()
+    {
+        $this->post(
+                    '/equipo',
+                    [
+                        'nombre' => 'MiEquipo',
+                        'entrenador' => 'Jorge',
+                        'categoria' => 'Master',
+                        'ids' => null,
+                    ]
+                    )
+             ->seeJson([
+                 'mensaje' => 'guardado con exito',
+             ]);
+    }
+
+    /**
+     * Comprueba el funcionamiento para crear un Equipo.
+     * Se crea un equipo y para guardar se envia por ajax los datos en formato JSON,
+     * se omite nombre del equipo, entrenador solo letras(Jorge),
+     * categoria(Master), sin jugadores.
+     * Es exitoso si el sistema responde con un estado 422 de las validaciones.
+     * Corresponde al caso de prueba testCrearEquipo: post-condition 1.
+     *
+     * @return void
+     */
+    public function testCrearEquipo3()
+    {
+        $response = $this->call(
+                    'POST',
+                    'equipo',
+                    [
+                        'nombre' => null,
+                        'entrenador' => 'Jorge',
+                        'categoria' => 'Master',
+                        'ids' => null,
+                    ]
+                    );
+
+        $this->assertEquals(422, $response->status());
+
+    }
+
+    /**
+     * Comprueba el funcionamiento para crear un Equipo.
+     * Se crea un equipo y para guardar se envia por ajax los datos en formato JSON,
+     * se ingresa nombre alfanumerico(MiEquipo), entrenador numeros(1234),
+     * categoria(Master), sin jugadores.
+     * Es exitoso si el sistema responde con un estado 422 de las validaciones.
+     * Corresponde al caso de prueba testCrearEquipo: post-condition 1.
+     *
+     * @return void
+     */
+    public function testCrearEquipo4()
+    {
+        $response = $this->call(
+                    'POST',
+                    'equipo',
+                    [
+                        'nombre' => 'MiEquipo',
+                        'entrenador' => '1234',
+                        'categoria' => 'Master',
+                        'ids' => '',
+                    ]
+                    );
+
+        $this->assertEquals(422, $response->status());
+
     }
 }
