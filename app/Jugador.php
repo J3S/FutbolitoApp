fb35eeef2a6bf8d34df787c05945f5c53f52a52e
Kevin Filella <kfilella@espol.edu.ec>
2016-08-21 23:45:29 -0500

    Jugadores por Equipo, estado 1

--- app/Jugador.php
+++ app/Jugador.php
@@ -8,7 +8,7 @@ class Jugador extends Model
 {
 
     public static function  getJugadoresxEquipo ($id_equipo) {
-        return Jugador::where('id_equipo', $id_equipo)->orderBy('rol', 'asc')->get();
+        return Jugador::where('id_equipo', $id_equipo)->orderBy('rol', 'asc')->where('estado', 1)->get();
     }
 
 }
